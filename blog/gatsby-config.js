8ade2688b89f2e59947c89cf74ebb10adc6af5ef
Alabe Duarte <alabeduarte@gmail.com>
2021-01-01 13:48:03 +1100

    Add trackingId

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-H8CTWTJ26R`,
       },
     },
     `gatsby-plugin-feed`,
