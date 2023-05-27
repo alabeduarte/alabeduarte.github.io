f5b10897ed94f84779021d82ad10af1ffdbe3a9a
Alabê Duarte <alabeduarte@gmail.com>
2023-05-27 13:07:06 +1000

    Remove tracking

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -51,12 +51,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-186320090-1`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
